cd24d4ec0bc46d68f047e61688279c09a322d24f
Ruslan GUSEINOV <guseynov.ruslan@gmail.com>
2022-04-24 14:00:15 +0200

    reimplement tests via pytest instead of unittest

--- test/test_io.py
+++ test/test_io.py
@@ -1,25 +1,18 @@
 import os
-import unittest
 
 import numpy as np
 
 from hole_filling_liepa.hole_filling_liepa import read_obj, write_obj
 
 
-class Test_obj(unittest.TestCase):
-
-    def test_read_obj(self):
-        vertices, faces = read_obj('../data/ico.obj')
-
-        self.assertEqual(len(vertices), 39)
-        self.assertEqual(len(faces), 67)
-
-    def test_write_obj(self):
-        file_path = '../data/tmp_test.obj'
-        vertices, faces = np.identity(3), [[0, 1, 2]]
-        write_obj(file_path, vertices, faces)
-        os.remove(file_path)
+def test_read_obj():
+    vertices, faces = read_obj('../data/ico.obj')
+    assert len(vertices) == 39
+    assert len(faces) == 67
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_write_obj():
+    file_path = '../data/tmp_test.obj'
+    vertices, faces = np.identity(3), [[0, 1, 2]]
+    write_obj(file_path, vertices, faces)
+    os.remove(file_path)
