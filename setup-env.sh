8c39e0c57c117e9fb1697b8f02a1ddf8e7ac1569
Ruslan Guseinov <guseynov.ruslan@gmail.com>
2022-04-29 11:47:02 +0200

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Install and configure Conda environment.
 # See https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html
-conda env create --file module/environment.yml --prefix ../env
+conda env create --file module/environment.yml --prefix ./env
 conda activate ./env
 conda config --env --set env_prompt '({name})'
 conda activate ./env
