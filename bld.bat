ecf06986ceb7c18163c8bb8207972b3601ed4b1d
Ruslan GUSEINOV <guseynov.ruslan@gmail.com>
2022-05-04 20:00:44 +0200

    use Ninja build system in Windows

--- bld.bat
+++ bld.bat
@@ -2,7 +2,9 @@ REM set "CXXFLAGS=%CXXFLAGS:-GL=%"
 REM set "CXXFLAGS= -MD"
 
 REM set "CMAKE_GENERATOR=NMake Makefiles JOM"
-set "CMAKE_GENERATOR=Visual Studio 15 2017"
+REM set "CMAKE_GENERATOR=Visual Studio 15 2017"
+set "CMAKE_GENERATOR=Ninja"
+
 "%PYTHON%" --version
 "%PYTHON%" -m pip install . --no-deps -vv
 
