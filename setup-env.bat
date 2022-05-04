649a44f2b23fc0002280d74816ef6ce77b0313f0
Ruslan GUSEINOV <guseynov.ruslan@gmail.com>
2022-05-04 12:26:18 +0200

    update setup-env

--- setup-env.bat
+++ setup-env.bat
@@ -1,8 +1,8 @@
 REM Install and configure Conda environment.
 @REM See https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html
-call conda env create --file module/environment.yml --prefix env
-call conda activate ./env
+call conda env create --file environment.yml --prefix ../hfl-env
+call conda activate ../hfl-env
 call conda config --env --set env_prompt ({name})
-call conda activate ./env
+call conda activate ../hfl-env
 call conda deactivate
 REM Installation finished.
--- setup-env.sh
+++ setup-env.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Install and configure Conda environment.
 # See https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html
-conda env create --file module/environment.yml --prefix ./env
-conda activate ./env
+conda env create --file environment.yml --prefix ../hfl-env
+conda activate ../hfl-env
 conda config --env --set env_prompt '({name})'
-conda activate ./env
+conda activate ../hfl-env
 conda deactivate
 echo Installation finished.
