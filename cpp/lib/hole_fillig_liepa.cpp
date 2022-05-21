8f85df11d8527af2adf2c4bd4cba2fef2d7c3e74
Ruslan GUSEINOV <guseynov.ruslan@gmail.com>
2022-05-21 21:25:39 +0200

    add missing const qualifiers

--- cpp/lib/hole_fillig_liepa.cpp
+++ cpp/lib/hole_fillig_liepa.cpp
@@ -27,7 +27,7 @@ namespace hole_filling_liepa {
 	typedef std::array<int, 2> array2i;
 	typedef std::array<int, 3> array3i;
 
-	vector<VectorXi> find_boundary_loops(const Ref<MatrixX3i> faces) {
+	vector<VectorXi> find_boundary_loops(const Ref<const MatrixX3i> faces) {
 		vector<VectorXi> boundary_loops;
 		set<std::pair<int, int>> edges;
 		for (int i = 0; i < faces.rows(); ++i) {
@@ -109,7 +109,7 @@ namespace hole_filling_liepa {
 		return Vector2i{ i, j };
 	}
 
-	Eigen::MatrixX3i fill_hole_liepa(const Ref<MatrixX3d> vertices, const Ref<MatrixX3i> faces, const Ref<VectorXi> boundary_loop, const std::string& method) {
+	Eigen::MatrixX3i fill_hole_liepa(const Ref<const MatrixX3d> vertices, const Ref<const MatrixX3i> faces, const Ref<const VectorXi> boundary_loop, const std::string& method) {
 		Eigen::MatrixX3i hole_triangles;
 
 		int n = boundary_loop.size();
